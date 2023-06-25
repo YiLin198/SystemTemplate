6734b81729624dda2d16cfce66544d19c0a55d76
chudc <1550758534@qq.com>
2023-06-25 22:43:21 +0800

    element-ui init

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from "vue";
+import ElementUI from "element-ui";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
 
 Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
+
+axios.defaults.withCredentials = true;
 
 new Vue({
   router,
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require("@vue/cli-service");
+const {defineConfig} = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true,
+    transpileDependencies: true,
+    publicPath: "./",
+    devServer: {
+        hot: true,
+        port: 8083, // 启动端口
+        // open: true,  // 启动后是否自动打开网页
+    },
 });
